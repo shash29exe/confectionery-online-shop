7905541e3887afe6c075e1265b33812f91a75303
shash29exe <zzxc67643@gmail.com>
2026-01-13 12:06:00 +0500

    Модели

--- myapp/models.py
+++ myapp/models.py
@@ -43,3 +43,40 @@ class ProductImage(models.Model):
 
     product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='products/')
+
+
+class Review(models.Model):
+    """
+        Класс отзывов
+    """
+
+    author = models.CharField('Автор', max_length=50)
+    text = models.TextField('Содержимое отзыва')
+    rating = models.PositiveSmallIntegerField('Оценка', default=5)
+    created_ad = models.DateTimeField('Дата создания', auto_now_add=True)
+
+    def __str__(self):
+        return f'Отзыв от {self.author}'
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        ordering = ['-created_at']
+
+
+class CartItem(models.Model):
+    """
+        Класс корзины
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Товар')
+    quantity = models.PositiveIntegerField('Количество', default=1)
+    created_at = models.DateTimeField('Дата добавления', auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.quantity} x {self.product.name}'
+
+    class Meta:
+        verbose_name = 'Элемент корзины'
+        verbose_name_plural = 'Элементы корзину'
