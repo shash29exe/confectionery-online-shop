72878605b21bea14fc66bd95af4577bfce020eb6
shash29exe <zzxc67643@gmail.com>
2026-01-24 13:59:56 +0500

    Views, шаблоны заглушки

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,17 @@
+import random
+from statistics import quantiles
+
 from django.contrib import messages
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, get_object_or_404, redirect
 import logging
 
 from django.views.generic import ListView
 
-from myapp.models import Product, Review, CartItem
+from myapp.forms import OrderForm
+from myapp.models import Product, Review, CartItem, OrderItem, Order
 
 logger = logging.getLogger(__name__)
 
@@ -18,13 +24,17 @@ def index(request):
                    'reviews': reviews})
 
 
-def product_detail(request, slug):
-    product = get_object_or_404(Product, slug=slug, is_available=True)
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk, is_available=True)
     return render(request, 'myapp/product_detail.html', {'product': product})
 
 
 def about(request):
     return render(request, 'myapp/about.html')
+
+
+def custom_404_view(request, exception):
+    return render(request, 'myapp/404.html', status=404)
 
 
 class ProductListView(ListView):
@@ -87,3 +97,68 @@ def reviews(request):
 
     reviews_list = Review.objects.all().order_by('-created_at')
     return render(request, 'myapp/reviews.html', {'reviews': reviews_list, 'title': 'Отзывы о нас'})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'myapp/register.html', {'form': form})
+
+
+@login_required
+def order_view(request):
+    cart_items = CartItem.objects.filter(user=request.user)
+    total = sum(item.product.price * item.quantity for item in cart_items)
+    if not cart_items:
+        return redirect('cart')
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            order.user = request.user
+            order.order_number = f'{random.randint(1, 100000)}'
+            order.save()
+
+            for item in cart_items:
+                OrderItem.objects.create(order=order, product=item.product, quantiy=item.quantity,
+                                         price=item.product.price)
+
+            cart_items.delete()
+
+            return redirect('order_success', order_id=order.id)
+
+    else:
+        form = OrderForm()
+
+    return render(request, 'myapp/order.html', {'form': form, 'cart_items': cart_items,
+                                                'total': total})
+
+
+@login_required
+def order_success(request, order_id):
+    order = get_object_or_404(Order, id=order_id, user=request.user)
+
+    return render(request, 'myapp/order_success.html', {'order': order})
+
+
+@login_required
+def order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id, user=request.user)
+
+    return render(request, 'myapp/order_detail.html', {'order': order})
+
+
+@login_required
+def order_history(request):
+    orders = Order.objects.filter(user=request.user).order_by('-created_at')
+
+    return render(request, 'myapp/order_history.html', {'orders': orders})
