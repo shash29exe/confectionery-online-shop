6e7d9f665fd737cd1b5013cadb0903f77a35746e
shash29exe <zzxc67643@gmail.com>
2026-02-05 11:55:00 +0500

    Запись логов в файл

--- confectionery/settings.py
+++ confectionery/settings.py
@@ -99,15 +99,31 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+
+    'formatters': {
+        'simple': {
+            'format': '[{asctime}] {levelname} {name}: {message}',
+            'style': '{',
+        },
+    },
+
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs', 'django.log'),
+            'formatter': 'simple',
         },
     },
+
     'loggers': {
         'myapp': {
-            'handlers': ['console'],
+            'handlers': ['console', 'file'],
             'level': 'INFO',
+            'propagate': False,
         },
     },
 }
