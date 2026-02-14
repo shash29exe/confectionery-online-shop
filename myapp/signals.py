3101b749a6feac4a70b7a0b14ac69cd3f83405c5
shash29exe <zzxc67643@gmail.com>
2026-02-14 14:02:27 +0500

    Время регистрации пользователя в signals

--- myapp/signals.py
+++ myapp/signals.py
@@ -3,15 +3,18 @@ from django.db.models.signals import post_save
 from django.core.mail import send_mail
 from django.conf import settings
 from django.dispatch import receiver
+from django.utils import timezone
 
 
 @receiver(post_save, sender=User)
 def notify_admin_about_new_user(sender, instance, created, **kwargs):
     if created:
+        local_time = timezone.localtime(instance.date_joined)
         subject = 'Новая регистрация'
         message = (
             f'Зарегистрирован новый пользователь\n'
-            f'Имя: {instance.username}, почта: {instance.email}'
+            f'Имя: {instance.username}, почта: {instance.email}\n'
+            f'Дата регистрации: {local_time.strftime("%Y-%m-%d %H:%M:%S")}'
         )
 
         send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [settings.ADMIN_EMAIL], fail_silently=True)
