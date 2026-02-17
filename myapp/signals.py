bb9f751ea9fd5b4a5d7eeb16a4dc3d023b0f8bb9
shash29exe <zzxc67643@gmail.com>
2026-02-17 11:48:35 +0500

    Новый сигнал о новом заказе

--- myapp/signals.py
+++ myapp/signals.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.dispatch import receiver
 from django.utils import timezone
 
+from myapp.models import Order
+
 
 @receiver(post_save, sender=User)
 def notify_admin_about_new_user(sender, instance, created, **kwargs):
@@ -15,6 +17,24 @@ def notify_admin_about_new_user(sender, instance, created, **kwargs):
             f'Зарегистрирован новый пользователь\n'
             f'Имя: {instance.username}, почта: {instance.email}\n'
             f'Дата регистрации: {local_time.strftime("%Y-%m-%d %H:%M:%S")}'
+        )
+
+        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [settings.ADMIN_EMAIL], fail_silently=True)
+
+
+@receiver(post_save, sender=Order)
+def notify_admin_about_new_order(sender, instance, created, **kwargs):
+    if created:
+        local_time = timezone.localtime(instance.created_at)
+        subject = 'Новый заказ'
+        message = (
+            f'Создан новый заказ!\n'
+            f'Номер заказа: {instance.order_number}\n'
+            f'ФИО: {instance.name}\n'
+            f'Номер телефона: {instance.phone}\n'
+            f'Время доставки: {instance.delivery_time or "Не указано"}\n'
+            f'Сумма заказа: {instance.total_price} ₽\n'
+            f'Дата заказа: {local_time.strftime("%Y-%m-%d %H:%M:%S")}\n'
         )
 
         send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [settings.ADMIN_EMAIL], fail_silently=True)
